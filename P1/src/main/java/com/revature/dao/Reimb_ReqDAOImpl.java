0d5f8b8c925b7cded8d89c23601436c8b5903261
stephenkk <[stephenkoffler@gmail.com]>
2019-09-23 19:13:45 -0400

    schema, beans, dao

---
+++ P1/src/main/java/com/revature/dao/Reimb_ReqDAOImpl.java
@@ -0,0 +1,14 @@
+package com.revature.dao;
+
+import java.util.List;
+
+import com.revature.beans.Reimb_Req;
+
+public class Reimb_ReqDAOImpl implements Reimb_ReqDAO {
+
+	public List<Reimb_Req> getAccounts(int ee_id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
